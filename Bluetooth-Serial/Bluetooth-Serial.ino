21223c10bad66d1e44b9c66035fd2c563c49deee
EricUlrich <ejulrich@gmail.com>
2016-10-24 14:33:45 -0700

    added RBG value via serial or bluetooth

--- Bluetooth-Serial/Bluetooth-Serial.ino
+++ Bluetooth-Serial/Bluetooth-Serial.ino
@@ -12,10 +12,33 @@ SoftwareSerial BTserial(2, 3); // RX | TX
 // Connect the HC-05 TX to Arduino pin 2 RX. 
 // Connect the HC-05 RX to Arduino pin 3 TX through a voltage divider.
 // 
- 
-//char c = ' ';
-byte c;
-#define ledPin 7 //LED PIN
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+
+char c = ' ';
+String intVal;
+#define ledPin 7 //LED PIN Normal LED
+#define rgbPIN 9 // Pin that will be used for the RGB strip
+#define rgdPIX 10 // Number of pixels in the strip
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = Arduino pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(rgdPIX, rgbPIN, NEO_GRB + NEO_KHZ800);
+
+enum
+{
+  kSetLed, // Command to request led to be set in specific state
+};
+
 void setup() 
 {
     Serial.begin(9600);
@@ -26,6 +49,9 @@ void setup()
     BTserial.begin(38400);
     // initialize the ledPin as an output:
     pinMode(ledPin, OUTPUT);
+
+    strip.begin();
+    strip.show(); // Initialize all pixels to 'off'
 }
  
 void loop()
@@ -34,50 +60,137 @@ void loop()
     // Keep reading from HC-05 and send to Arduino Serial Monitor
     if (BTserial.available())
     {  
-        c = BTserial.read();
-        Serial.write(c);
-        switch (c){
-          case 1:
-            digitalWrite(ledPin, HIGH);
-            break;
-          case 2:
-            digitalWrite(ledPin, LOW);
-            break;
-          case 3:
-            digitalWrite(ledPin, HIGH);
-            delay(100);
-            digitalWrite(ledPin, LOW);
-            delay(100);
-           default:
-            digitalWrite(ledPin, HIGH);
-            delay(100);
-            digitalWrite(ledPin, LOW);
-            delay(100);
-            break;
-        }
+        intVal = BTserial.read();
+        Serial.print(intVal);
+        BTserial.print(intVal);
+        BTserial.print('\n');
+        String redVal = getValue(String(intVal), ':', 0);
+        String blueVal = getValue(String(intVal), ':', 1);
+        String greenVal = getValue(String(intVal), ':', 2);
+        BTserial.println(redVal);
+        BTserial.println(blueVal);
+        BTserial.println(greenVal);
+        colorWipe(strip.Color(redVal.toInt(), blueVal.toInt(), greenVal.toInt()), 50);
     }
  
     // Keep reading from Arduino Serial Monitor and send to HC-05
     if (Serial.available())
     {
-        c =  Serial.read();
-        BTserial.write(c); 
-        switch (c){
-          case 1:
-            digitalWrite(ledPin, HIGH);
-            break;
-          case 2:
-            digitalWrite(ledPin, LOW);
-            break;
-          case 3:
-            digitalWrite(ledPin, HIGH);
-            delay(1000);
-            digitalWrite(ledPin, LOW);
-            delay(1000);
-           default:
-      
-           break;
-        } 
+        intVal = Serial.readString();
+        BTserial.print(intVal); 
+        Serial.print(intVal);
+        Serial.print('\n');
+        String redVal = getValue(String(intVal), ':', 0);
+        String blueVal = getValue(String(intVal), ':', 1);
+        String greenVal = getValue(String(intVal), ':', 2);
+        Serial.println(redVal);
+        Serial.println(blueVal);
+        Serial.println(greenVal);
+        colorWipe(strip.Color(redVal.toInt(), blueVal.toInt(), greenVal.toInt()), 50);
     }
- 
 }
+
+
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
+}
+
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+//Theatre-style crawling lights.
+void theaterChase(uint32_t c, uint8_t wait) {
+  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
+    for (int q=0; q < 3; q++) {
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, c);    //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
+
+//Theatre-style crawling lights with rainbow effect
+void theaterChaseRainbow(uint8_t wait) {
+  for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
+    for (int q=0; q < 3; q++) {
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
+
+String getValue(String data, char separator, int index)
+{
+ int found = 0;
+  int strIndex[] = {
+0, -1  };
+  int maxIndex = data.length()-1;
+  for(int i=0; i<=maxIndex && found<=index; i++){
+  if(data.charAt(i)==separator || i==maxIndex){
+  found++;
+  strIndex[0] = strIndex[1]+1;
+  strIndex[1] = (i == maxIndex) ? i+1 : i;
+  }
+ }
+  return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
+
+
